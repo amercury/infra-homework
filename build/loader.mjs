6e51562edf9ea55d9e9f799c6e7bee37f19d79a7
amercury <alexigov@gmail.com>
2024-12-05 22:24:45 +0100

    feat(loader.mjs): added logic

--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,24 @@
 import yaml from 'yaml';
 import path from 'node:path';
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
+
+export async function load(url, context, nextLoad) {
+  const ext = url
+    .split('.')
+    .at(-1);
+
+  if (ext === 'yml') {
+    const normalizedUrl = url
+      .split('://')
+      .at(-1);
+    const source = fs.readFileSync(path.resolve(normalizedUrl), 'utf8');
+
+    return {
+      format: 'json',
+      shortCircuit: true,
+      source: JSON.stringify(yaml.parse(source)),
+    }
+  }
+
+  return nextLoad(url, context);
+}
