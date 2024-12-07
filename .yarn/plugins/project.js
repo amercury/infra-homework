5e8e51e07f3d3ca8c91da255dd7b78107f7628a4
amercury <alexigov@gmail.com>
2024-12-07 17:35:20 +0100

    feat(yarn): added my custom plugin

--- .yarn/plugins/project.js
+++ .yarn/plugins/project.js
@@ -1,6 +1,37 @@
 module.exports = {
   name: `plugin-project`,
   factory: require => {
-    return {};
+    const {BaseCommand} = require(`@yarnpkg/cli`);
+    const {Manifest} = require(`@yarnpkg/core`);
+
+    class GetProjectInfoCommand extends BaseCommand {
+      static paths = [[`project`]];
+
+      async getInfo() {
+        return await Manifest.find('').then(data => {
+          const dependencies =
+            Object.entries(data.raw?.dependencies)
+            .map(([key, value]) => `${key}: ${value}`);
+
+          return JSON.stringify({
+            name: data.raw?.name,
+            version: data.raw?.version,
+            description: data.raw?.description,
+            dependencies
+          });
+        });
+      }
+
+      async execute() {
+        const info = await this.getInfo();
+        this.context.stdout.write(info);
+      }
+    }
+
+    return {
+      commands: [
+        GetProjectInfoCommand,
+      ],
+    };
   }
 };
